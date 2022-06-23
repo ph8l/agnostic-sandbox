61fb78138c30d07b04e2e95b73db95ea712026ee
Joe Jackson <ph8tel@gmail.com>
2022-06-22 20:21:19 -0700

    Form ui

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static('build'))
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.get('/test', (req, res) => { 
   res.send('test route')
 })
 app.listen(port, () => {
-    
+
     console.log(`listening on port ${port}`)
 })
